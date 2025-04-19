60cead5df07a411ef3526b2e57a81310096f3d78
Lukey721 <lukeyconnolly@hotmail.com>
2025-04-19 20:28:55 +0100

    updates for ruby to handle

--- user-api/config/initializers/prometheus_exporter.rb
+++ user-api/config/initializers/prometheus_exporter.rb
@@ -1,9 +1,6 @@
 require 'prometheus_exporter/middleware'
-require 'prometheus_exporter/server'
 
-# This attaches rack middleware to track request metrics
+# Attach middleware for automatic request metrics
 Rails.application.middleware.unshift PrometheusExporter::Middleware
 
-# Starts a standalone metrics web server 
-PrometheusExporter::Server::WebServer.start bind: '0.0.0.0', port: 9394
-puts "[PrometheusExporter] Exporter started on port 9394"
+puts "[PrometheusExporter] Middleware initialized"
