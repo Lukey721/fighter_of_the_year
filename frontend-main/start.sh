01ea3d93b8ce30e2de9be9319594a2706a83f1d0
Lukey721 <lukeyconnolly@hotmail.com>
2025-04-21 17:29:16 +0100

    updated to form to send correctly to proxy

--- frontend-main/start.sh
+++ frontend-main/start.sh
@@ -14,10 +14,13 @@ server {
 
   location / {
     proxy_pass http://$PROXY_TARGET;
+    proxy_http_version 1.1;
     proxy_set_header Host \$host;
     proxy_set_header X-Real-IP \$remote_addr;
     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     proxy_set_header X-Forwarded-Proto \$scheme;
+    client_max_body_size 10M;
+    proxy_buffering off;
   }
 }
 EOF
