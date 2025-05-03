e3aea52031076c6a8908443281b2bca6153ddfde
Lukey721 <lukeyconnolly@hotmail.com>
2025-05-03 16:13:40 +0100

    reduce terraform role and replace with swarm

--- user-api/config/initializers/prometheus_exporter.rb
+++ user-api/config/initializers/prometheus_exporter.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
-
 require 'prometheus_exporter/middleware'
 require 'prometheus_exporter/server'
 
 Rails.application.middleware.unshift PrometheusExporter::Middleware
 
-# ✅ CORRECT usage of exporter server
 server = PrometheusExporter::Server::WebServer.new bind: '0.0.0.0', port: 9394
 server.start
